00ebfbeafcf99834138af84335908871fd3e32d8
Charat  Thepsoh <nuna@kimnb.localdomain>
2018-02-07 21:21:25 +0700

    CRUD

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -12,6 +12,54 @@ exports.create = function(req, res, next) {
     });
 };
 
+exports.list = function(req, res, next) {
+    User.find({}, function(err, users) {
+        if(err) {
+            next(err);
+        } else {
+            res.json(users);
+        }
+    });
+};
+
+exports.read = function(req, res) {
+    res.json(req.user);
+};
+
+exports.update = function(req, res, next) {
+    User.findOneAndUpdate({username: req.user.username}, req.body,
+        function(err, user) {
+            if(err) {
+                next(err);
+            } else {
+                res.json(user);
+            }
+        });
+};
+
+exports.delete = function(req, res, next) {
+    req.user.remove(function(err) {
+        if(err) {
+            next(err);
+        } else {
+            res.json(req.user);
+        }
+    });
+};
+
+exports.userByUsername = function(req, res, next, username) {
+    User.findOne({
+        username: username
+    }, function(err, user) {
+        if(err) {
+            next(err);
+        } else {
+            req.user = user;
+            next();
+        }
+    });
+};
+
 exports.login = function(req, res) {
     req.checkBody('email', 'Invalid email').notEmpty().isEmail();
     req.sanitizeBody('email').normalizeEmail();
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -4,9 +4,13 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema ({
     firstName: String,
     lastName: String,
-    username: {type: String, unique: true},
+    username: {type: String, unique: true, trim: true},
     email: {type: String, index: true},
-    password: String
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 mongoose.model('User', UserSchema);
