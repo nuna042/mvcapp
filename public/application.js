d3925710d147d45d17113ba65ecddeb2008f0a36
Charat  Thepsoh <nuna@kimnb.localdomain>
2018-02-27 23:31:58 +0700

    ui-router

--- public/application.js
+++ public/application.js
@@ -1,18 +1,8 @@
 var mainAppModuleName = 'Main';
-var mainAppModule = angular.module(mainAppModuleName, ['hello']);
+var mainAppModule = angular.module(mainAppModuleName, ['ui.router', 'hello']);
 
 angular.element(document).ready(function() {
     angular.bootstrap(document.querySelector('#mainApp'), [mainAppModuleName], {
         strictDi: true
     });
-});
-
-mainAppModule.controller('NameController', ['$scope', function($scope) {
-    $scope.yourName = 'No Name';
-}]);
-
-mainAppModule.filter('sayhello', function() {
-    return function(name) {
-        return 'Hello, ' + name;
-    };
 });
